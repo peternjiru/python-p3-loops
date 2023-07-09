272eb5c215b7af25941fb0ebc2244f5d9e1d8877
Peter Njiru <peternjirum@gmail.com>
2023-07-09 17:03:51 -0400

    completes loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count != 0:
+        if count == 1:
+            print(count)
+            print("Happy New Year!")
+        else:
+            print(count)
+        count = count - 1
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_ints = [num * num for num in int_list]
+    return squared_ints
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while num <= 100:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        num = num + 1
+    
+fizzbuzz()
